acb33cbe90317efe7907b58a21e178c754d18522
Romaim Gianfranco Talledo Coronado <rogitaco@gmail.com>
2024-10-11 11:08:35 -0500

    estructura completa

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,43 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/home/main/dashboard/dashboard.component';
+import { ProyectosComponent } from './pages/home/main/proyectos/proyectos.component';
 
 export const routes: Routes = [
 
   {
     path:'',
     component: HomeComponent,
+    children:[
+      {
+        path: '',
+        loadComponent: () => import('./pages/home/main/dashboard/dashboard.component').then(m => m.DashboardComponent)
+      },
+      {
+        path: 'proyectos',
+        loadComponent:()=> import('./pages/home/main/proyectos/proyectos.component').then(m=>m.ProyectosComponent)
+      },
+      {
+        path: 'tareas',
+        loadComponent: () => import('./pages/home/main/tareas/tareas.component').then(m => m.TareasComponent)
+      },
+      {
+        path: 'equipo',
+        loadComponent: () => import('./pages/home/main/equipo/equipo.component').then(m => m.EquipoComponent)
+      },
+      {
+        path: 'reportes',
+        loadComponent: () => import('./pages/home/main/reportes/reportes.component').then(m => m.ReportesComponent)
+      },
+      {
+        path:'clientes',
+        loadComponent:()=> import('./pages/home/main/clientes/clientes.component').then(m=>m.ClientesComponent)
+      },
+      {
+        path:'servicios',
+        loadComponent:()=> import('./pages/home/main/servicios/servicios.component').then(m=>m.ServiciosComponent)
+      }
+    ]
 
   }
 
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -2,14 +2,48 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from "./sidebar/sidebar.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { MainComponent } from "./main/main.component";
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SidebarComponent, NavbarComponent, MainComponent],
+  imports: [SidebarComponent, NavbarComponent, MainComponent, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  title: string = 'Dashboard';
 
+  constructor(private router: Router) {
+    this.router.events.subscribe(event=>{
+      if(event instanceof NavigationEnd){
+       switch(event.url){
+        case '/':
+          this.title = 'Dashboard';
+          break;
+        case '/proyectos':
+          this.title = 'Proyectos';
+          break;
+        case '/tareas':
+          this.title = 'Tareas';
+          break;
+          case '/equipo':
+          this.title = 'Equipo';
+          break;
+          case '/reportes':
+          this.title = 'Reportes';
+          break;
+          case '/clientes':
+          this.title = 'Clientes';
+          break;
+          case '/servicios':
+          this.title = 'Servicios';
+          break;
+          default:
+          this.title = 'Dashboard';
+          break;
+        }
+      }
+    })
+  }
 }
