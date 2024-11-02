742af2929cb38f868bc71d3fb5660ec5ceb18c22
Romaim Gianfranco Talledo Coronado <rogitaco@gmail.com>
2024-11-01 21:58:19 -0500

    CRUD PROYECTOS TAREAS

---
+++ frontend/src/app/pages/home/main/tareas/nueva-subtarea/nueva-subtarea.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nueva-subtarea',
+  standalone: true,
+  imports: [],
+  templateUrl: './nueva-subtarea.component.html',
+  styleUrl: './nueva-subtarea.component.css'
+})
+export class NuevaSubtareaComponent {
+
+}
